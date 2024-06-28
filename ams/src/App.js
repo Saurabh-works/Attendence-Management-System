349e8ac4b78da43929ee8819c49ca8c894d2d10a
Saurabh <saurabhshinde9637@gmail.com>
2024-06-28 20:35:51 +0530

    Committing changes in installation

--- ams/src/App.js
+++ ams/src/App.js
@@ -5,14 +5,10 @@ import './index.css';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>App</h1>
       <LoginIndex></LoginIndex>
-=======
-      <Login_index></Login_index>
->>>>>>> 7e42c974d54201bfdbbf8e87336408e342774a18
     </>
   );
 }
+
 
 export default App;
